372b2f91db1f7ee37b2de0eaffbb0d667491be5e
Abdullah Süha ışık <abdullahsuhaisik@gmail.com>
2022-09-29 10:01:54 +0300

    logout clear data

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { BiExit } from "react-icons/bi";
-
+import { Context as JobContext } from '../../hooks/JobContext';
 
 import './Header.css'
 
 export const Header = ({ userLogged, logout }: any) => {
+  const { clearJob } = useContext(JobContext);
+
   return (
     <div className='header'>
       <div className="flex-container-header">
         {
           !userLogged ? null : <div className="logout-button flex" onClick={() => {
-            logout()
+            logout();
+            clearJob();
           }}>
             <BiExit style={{
               color: "rgb(255, 255, 255)",
