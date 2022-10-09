2b74c16d7fd1310aef732d1b33178825c20c449c
Merve Bacak <nmerveb@gmail.com>
2022-10-09 23:37:58 +0300

    Qr code generator and printer added.

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Button.css";
 
-const Button = ({ styles, handleClick, title, primary, isDisabled }: any) => (
+const Button = ({
+  styles,
+  handleClick,
+  title,
+  primary,
+  isDisabled,
+  btnType,
+}: any) => (
   <button
     onClick={handleClick}
-    type="button"
+    type={btnType ? btnType : "button"}
     className={primary ? "btn btn-primary" : "btn btn-secondary"}
     disabled={isDisabled}
   >
